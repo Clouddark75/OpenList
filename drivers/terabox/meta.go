df22467822f2b167b9526a39c54b1b5bb32bc729
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2025-06-21 12:45:38 -0400

    Update meta.go
    Signed-off-by: Clouddark75 <84534855+Clouddark75@users.noreply.github.com>

--- drivers/terabox/meta.go
+++ drivers/terabox/meta.go
@@ -9,6 +9,7 @@ type Addition struct {
 	driver.RootPath
 	Cookie string `json:"cookie" required:"true"`
 	//JsToken        string `json:"js_token" type:"string" required:"true"`
+	UserAgent      string `json:"user_agent" required:"true" default:"terabox;1.40.0.132;PC;PC-Windows;10.0.26100;Windows TeraBox"`
 	DownloadAPI    string `json:"download_api" type:"select" options:"official,crack" default:"official"`
 	OrderBy        string `json:"order_by" type:"select" options:"name,time,size" default:"name"`
 	OrderDirection string `json:"order_direction" type:"select" options:"asc,desc" default:"asc"`
