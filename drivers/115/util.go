c47a335ba8849293f1cd6e1ae353ddebfa9aeae0
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2025-08-12 16:45:43 -0400

    Update util.go
    Signed-off-by: Clouddark75 <84534855+Clouddark75@users.noreply.github.com>

--- drivers/115/util.go
+++ drivers/115/util.go
@@ -24,9 +24,9 @@ import (
 	"github.com/OpenListTeam/OpenList/v4/pkg/utils"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 
-	cipher "github.com/SheltonZhu/115driver/pkg/crypto/ec115"
-	crypto "github.com/SheltonZhu/115driver/pkg/crypto/m115"
-	driver115 "github.com/SheltonZhu/115driver/pkg/driver"
+	cipher "github.com/Clouddark75/115driver/pkg/crypto/ec115"
+	crypto "github.com/Clouddark75/115driver/pkg/crypto/m115"
+	driver115 "github.com/Clouddark75/115driver/pkg/driver"
 	"github.com/pkg/errors"
 )
 
