a0b6ee741410866b6adbb1d2df5b2acb25b340ff
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2025-10-13 16:49:49 -0400

    Update meta.go
    Signed-off-by: Clouddark75 <84534855+Clouddark75@users.noreply.github.com>

--- drivers/webdav/meta.go
+++ drivers/webdav/meta.go
@@ -10,6 +10,7 @@ type Addition struct {
 	Address  string `json:"address" required:"true"`
 	Username string `json:"username" required:"true"`
 	Password string `json:"password" required:"true"`
+	TenantID string `json:"tenant_id" help:"Opcional para SharePoint: ID del tenant (ej: 83daeb17-e8de-45a3-ae70-69c9dbdf4cbf). Déjalo vacío para auto-detectar."`
 	driver.RootPath
 	TlsInsecureSkipVerify bool `json:"tls_insecure_skip_verify" default:"false"`
 }
